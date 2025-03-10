61d4c6e00d404fc87f69e2a949982ae9b180061c
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-03-10 14:49:22 +0700

    task4.5

--- src/function.ts
+++ src/function.ts
@@ -1,4 +1,9 @@
-const add = (a:number,b:number):string => {
-    const result =  a+b;
+export const add = (a: number, b: number): string => {
+    const result = a + b;
     return result.toString();
- }
+}
+
+export const substract = (a:number,b:number) : string => {
+    const result = a - b;
+    return result.toString();
+}
--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,19 @@
 // }
 
 //Task 3
+import { add } from './function';
+//const result = add(1,2) + 0;
 
-const result = add(1,2) + 0;
-console.log(result, 'type of result: ', typeof result);
+
+//console.log(result, 'type of result:', typeof result);
+
+import {substract} from'./function';
+const result = add(1, 2) + 0;
+const result2 = substract(1,2) + 0;
+console.log(result, 'type of result:', typeof result);
+console.log(result2, 'type of result:', typeof result2);
+//const result = add(1,2) + 0;
+//console.log(result, 'type of result: ', typeof result);
  //findMaX Function
 //const findMax = (numbers: number[]): string => {
 //    const maxNumber = Math.max(...numbers);
