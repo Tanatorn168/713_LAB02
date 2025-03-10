6ce075d97e84a4c6d4b479254b77f8a3c9dbe7a3
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-03-10 13:18:28 +0700

    task3.1

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 //console.log("Hello, TypeScript with Node.js!");
-let x:number | string = 10;
-x = 10;
+//let x:number | string = 10;
+//x = 10;
 
-if (typeof x === 'string') {
-    console.log('x is a string');
- }else if (typeof x === 'number') {
-    console.log('x is a number');
- }else {
-    console.log('x is neither a string nor a number');
- }
- 
+//if (typeof x === 'string') {
+//    console.log('x is a string');
+// }else if (typeof x === 'number') {
+//   console.log('x is a number');
+// }else {
+//    console.log('x is neither a string nor a number');
+// }
+
+//Task 3
+const add = (a,b) => {
+    return a+b;
+}
+console.log(add(1,2));
