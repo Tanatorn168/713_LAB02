9067347824a8d814df2a1c0b975f5de48474b9e5
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-03-14 10:40:50 +0700

    task8.9

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,12 @@ import express, { Request, Response } from 'express'
 const app = express()
 const port = 3000
 
-app.get('/test', (req, res) => {
-    let returnObj = {
-        name: 'test',
-        age: 20,
-        address: 'Thai'
-    }
-    res.send(returnObj);
+app.get('/test', (req: Request, res: Response) => {
+    const id = req.query.id;
+    const output = `id: ${id}`;
+    res.send(output);
 })
+
    
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
